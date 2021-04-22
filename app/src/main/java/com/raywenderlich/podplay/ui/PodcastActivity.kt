1b1b5ae3cb6ca713bca0e897f0882ea35b29327e
AlexLyt <aleks.zahid@gmail.com>
2021-04-22 18:59:29 -0400

    Chapter 21. Completed.

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -1,13 +1,30 @@
 package com.raywenderlich.podplay.ui
 
+import android.app.SearchManager
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.adapter.PodcastListAdapter
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.ItunesService
+import com.raywenderlich.podplay.viewmodel.SearchViewModel
+import kotlinx.android.synthetic.main.activity_podcast.*
 
-class PodcastActivity : AppCompatActivity() {
+// MY PROJECT
+class PodcastActivity : AppCompatActivity(), PodcastListAdapter.PodcastListAdapterListener {
+
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -19,5 +36,82 @@ class PodcastActivity : AppCompatActivity() {
         }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+        setupToolbar()
+        setupViewModels()
+        updateControls()
+        handleIntent(intent)
     }
+
+    // Search widget
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE)
+                as SearchManager
+        searchView.setSearchableInfo(
+            searchManager.getSearchableInfo(componentName)
+        )
+        return true
+    }
+
+    private fun performSearch(term: String) {
+        showProgressBar()
+        searchViewModel.searchPodcasts(term) { results ->
+            hideProgressBar()
+            toolbar.title = term
+            podcastListAdapter.setSearchData(results)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+    }
+// set up view models
+    private fun setupViewModels() {
+        val service = ItunesService.instance
+        searchViewModel.iTunesRepo = ItunesRepo(service)
+    }
+
+    private fun updateControls() {
+        podcastRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        podcastRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(podcastRecyclerView.context,
+            layoutManager.orientation)
+        podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+
+        podcastListAdapter = PodcastListAdapter(null,this, this)
+        podcastRecyclerView.adapter = podcastListAdapter
+    }
+// called when the user taps on a podcast
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
+
+    override fun onShowDetails(
+        podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData
+    ) {
+// Not implemented yet
+    }
+
 }
